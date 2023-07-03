58381654aeac7d792d0e0ab36630e20e69a4f24a
DroWWarD <chernov_da@itgit.ru>
2023-07-03 14:27:49 +0300

    Added TextWrapping for cells in searchResult file

--- src/main/java/tLogistic/services/implementations/SearchingServiceImpl.java
+++ src/main/java/tLogistic/services/implementations/SearchingServiceImpl.java
@@ -1,6 +1,7 @@
 package tLogistic.services.implementations;
 
 import com.aspose.cells.Cells;
+import com.aspose.cells.Style;
 import com.aspose.cells.Workbook;
 import com.aspose.cells.Worksheet;
 import lombok.RequiredArgsConstructor;
@@ -41,11 +42,26 @@ public class SearchingServiceImpl implements SearchingService {
             File file = new File("src/main/resources/templates/searchErrorClient.html");
             return ResponseEntity.badRequest().body(new ByteArrayResource(new FileInputStream(file).readAllBytes()));
         }
+        Workbook workbook = new Workbook();
+        Worksheet worksheetResult = workbook.getWorksheets().get(0);
+        worksheetResult.setName("Результаты поиска");
+        Worksheet worksheetMissing = workbook.getWorksheets().add("Нет в БД");
+        findArticlesAndFillWorkSheets(worksheetResult,worksheetMissing,articles, optionalClient.get());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(" dd-MM-yyyy HH:mm ");
+        File tempFile = File.createTempFile("searchResult" + LocalDateTime.now().format(formatter), ".xlsx");
+        workbook.save(String.valueOf(tempFile));
+        tempFile.deleteOnExit();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + tempFile.getName() + "\"")
+                .body(new ByteArrayResource(new FileInputStream(tempFile).readAllBytes()));
+    }
+
+    private void findArticlesAndFillWorkSheets(Worksheet worksheetResult, Worksheet worksheetMissing, String articles, Client client) {
         articles = articles.replaceAll("\"", "");
         List<Article> searchResult = new ArrayList<>();
         List<String> missingArticles = new ArrayList<>();
         String[] articlesFromRequest = articles.split("[\\n]+");
-        Iterable<Article> articlesInBase = articleRepository.findAllByClient(optionalClient.get());
+        Iterable<Article> articlesInBase = articleRepository.findAllByClient(client);
         Map<String, Article> map = new HashMap<>();
         for (Article a : articlesInBase) {
             map.put(a.getArticle(), a);
@@ -61,12 +77,35 @@ public class SearchingServiceImpl implements SearchingService {
                 searchResult.add(map.get(s));
             }
         }
-        Workbook workbook = new Workbook();
-        Worksheet worksheetResult = workbook.getWorksheets().get(0);
-        worksheetResult.setName("Результаты поиска");
-        Worksheet worksheetMissing = workbook.getWorksheets().add("Нет в БД");
         Cells cellsResult = worksheetResult.getCells();
         Cells cellsMissing = worksheetMissing.getCells();
+        setColumnsFormat(cellsMissing,cellsResult);
+        fillCells(cellsMissing,cellsResult,missingArticles,searchResult);
+    }
+
+    private void fillCells(Cells cellsMissing, Cells cellsResult, List<String> missingArticles, List<Article> searchResult) {
+        if (!missingArticles.isEmpty()) {
+            cellsMissing.get(0, 0).putValue("Следующие артикулы отсутствуют в БД");
+            for (int i = 0; i < missingArticles.size(); i++) {
+                cellsMissing.get(i + 1, 0).putValue(missingArticles.get(i));
+            }
+        }
+        if (!searchResult.isEmpty()) {
+            for (int i = 0; i < searchResult.size(); i++) {
+                Article article = searchResult.get(i);
+                cellsResult.get(i, 0).putValue(article.getArticle());
+                cellsResult.get(i, 1).putValue(article.getDescription());
+                cellsResult.get(i, 2).putValue(article.getCode());
+                cellsResult.get(i, 3).putValue(article.getSupportingDoc());
+                cellsResult.get(i, 4).putValue("");
+            }
+        }
+    }
+
+    private void setColumnsFormat(Cells cellsMissing, Cells cellsResult) {
+        Style style = cellsResult.getStyle();
+        style.setTextWrapped(true);
+        cellsResult.setStyle(style);
         cellsMissing.setColumnWidth(0, 50);
         cellsMissing.setColumnWidth(1, 50);
         cellsResult.setColumnWidth(0, 15);
@@ -74,29 +113,5 @@ public class SearchingServiceImpl implements SearchingService {
         cellsResult.setColumnWidth(2, 15);
         cellsResult.setColumnWidth(3, 50);
         cellsResult.setColumnWidth(4, 20);
-        if (!missingArticles.isEmpty()) {
-            worksheetMissing.getCells().get(0, 0).putValue("Следующие артикулы отсутствуют в БД");
-            for (int i = 0; i < missingArticles.size(); i++) {
-                worksheetMissing.getCells().get(i + 1, 0).putValue(missingArticles.get(i));
-            }
-        }
-        if (!searchResult.isEmpty()) {
-            for (int i = 0; i < searchResult.size(); i++) {
-                Article article = searchResult.get(i);
-                worksheetResult.getCells().get(i, 0).putValue(article.getArticle());
-                worksheetResult.getCells().get(i, 1).putValue(article.getDescription());
-                worksheetResult.getCells().get(i, 2).putValue(article.getCode());
-                worksheetResult.getCells().get(i, 3).putValue(article.getSupportingDoc());
-                worksheetResult.getCells().get(i, 4).putValue("");
-            }
-        }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(" dd-MM-yyyy HH:mm ");
-        File tempFile = File.createTempFile("searchResult" + LocalDateTime.now().format(formatter), ".xlsx");
-        workbook.save(String.valueOf(tempFile));
-        tempFile.deleteOnExit();
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + tempFile.getName() + "\"")
-                .body(new ByteArrayResource(new FileInputStream(tempFile).readAllBytes()));
     }
 }
